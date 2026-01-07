a084e6d841bad2750fbc92e32bbdc1f1787473bd
CodeCures <couragesblog@gmail.com>
2026-01-07 23:11:16 +0100

    Add API resources for article responses

---
+++ backend/app/Http/Resources/ArticleResource.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class ArticleResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'title' => $this->title,
+            'description' => $this->description,
+            'content' => $this->content,
+            'url' => $this->url,
+            'image_url' => $this->image_url,
+            'author' => $this->author,
+            'published_at' => $this->published_at,
+            'source' => [
+                'id' => $this->source->id,
+                'name' => $this->source->name,
+                'slug' => $this->source->slug,
+            ],
+            'category' => $this->category ? [
+                'id' => $this->category->id,
+                'name' => $this->category->name,
+                'slug' => $this->category->slug,
+            ] : null,
+            'created_at' => $this->created_at,
+        ];
+    }
+}
