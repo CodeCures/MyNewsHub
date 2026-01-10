25b3af6ef84b3b5cb480e0d891119465ec86fe90
CodeCures <couragesblog@gmail.com>
2026-01-10 16:07:59 +0100

    refactor: remove unused methods

--- backend/app/Http/Controllers/Api/UserPreferenceController.php
+++ backend/app/Http/Controllers/Api/UserPreferenceController.php
@@ -44,28 +44,4 @@ class UserPreferenceController extends Controller
 
         return response()->json($preference, 201);
     }
-
-    /**
-     * Display the specified resource.
-     */
-    public function show(string $id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, string $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(string $id)
-    {
-        //
-    }
 }
